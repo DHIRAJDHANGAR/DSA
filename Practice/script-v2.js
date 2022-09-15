478d049358e2519840a9debf5a30bbfaccbd7fce
Aakash <dhirajdesale2021@gmail.com>
2022-09-15 20:41:11 +0530

    nestedObjectV2

--- Practice/script-v2.js
+++ Practice/script-v2.js
@@ -29,12 +29,12 @@ const findPath = (object, path) => {
   return object;
 };
 findPath(obj, "a.b.c");
-// console.log(findPath(obj, "a.b.c")); // 12
-// console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
-// console.log(findPath(obj, 'a.b.d')); // undefined
-// console.log(findPath(obj, 'a.c')); // undefined
-// console.log(findPath(obj, 'a.b.c.d')); // undefined
-// console.log(findPath(obj, 'a.b.c.d.e')); // undefined
-// console.log(findPath(obj, 'a.b.j')); //false
-// console.log(findPath(obj, 'a.b.j.k')); //undefined
-// console.log(findPath(obj, 'a.k')); //null
+console.log(findPath(obj, "a.b.c")); // 12
+console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+console.log(findPath(obj, 'a.b.d')); // undefined
+console.log(findPath(obj, 'a.c')); // undefined
+console.log(findPath(obj, 'a.b.c.d')); // undefined
+console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+console.log(findPath(obj, 'a.b.j')); //false
+console.log(findPath(obj, 'a.b.j.k')); //undefined
+console.log(findPath(obj, 'a.k')); //null
