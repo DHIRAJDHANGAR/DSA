c919a439827a6dfb4f72afc7ff6121e915e73aff
Aakash <dhirajdesale2021@gmail.com>
2022-09-15 20:39:16 +0530

    nestedObjectV2

---
+++ Day-5/handle-nested-object/nestedObjectV2.js
@@ -0,0 +1,41 @@
+/*
+- Write method findPath
+- Should take two params:
+    - object
+    - keys separated by dots as string
+- Return value if it exists at that path inside the object, else return undefined
+*/
+
+const obj = {
+    a: {
+      b: {
+        c: 12,
+        j: false,
+      },
+      k: null,
+    },
+  };
+  
+  const findPath = (object, path) => {
+    let rec = path.split(".");
+  
+    for (let i = 0; i < rec.length; i++) {
+      if (object.hasOwnProperty(rec[i])) {
+        object = object[rec[i]];
+      } else {
+        return undefined;
+      }
+    }
+    return object;
+  };
+  findPath(obj, "a.b.c");
+  console.log(findPath(obj, "a.b.c")); // 12
+//   console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+//   console.log(findPath(obj, 'a.b.d')); // undefined
+//   console.log(findPath(obj, 'a.c')); // undefined
+//   console.log(findPath(obj, 'a.b.c.d')); // undefined
+//   console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+//   console.log(findPath(obj, 'a.b.j')); //false
+//   console.log(findPath(obj, 'a.b.j.k')); //undefined
+//   console.log(findPath(obj, 'a.k')); //null
+  
